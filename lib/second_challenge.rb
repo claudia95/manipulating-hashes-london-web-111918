b0b75f82ba0eab6ad55675ecf86fa9ba0826d361
Claudia Fox <claudiafox95@icloud.com>
2018-11-13 22:38:58 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -9,6 +9,9 @@ def second_challenge
   #code your solution here!
 
 dairy_array = second_challenge[groceries][dairy].values
-  
+vegetable_array = second_challenge[groceries][vegetable].values
+meat_array = second_challenge[groceries][meat].values
+grains_array = second_challenge[groceries][grains].values
 
+groceries_array = 
 end
